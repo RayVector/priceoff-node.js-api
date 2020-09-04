12c8acd145508fb1998822bc16ca67254db21b56
rayvector <rayvectorspqr@yahoo.com>
2020-09-04 16:57:17 +0300

    refactor user+ categories

--- index.js
+++ index.js
@@ -1,15 +1,12 @@
 const express = require('express');
-const app = express();
 const config = require('./configs/main');
 const {port, apiUrl} = config;
 
-// require controllers:
-const userController = require('./controller/userController');
-const categoriesController = require('./controller/categoriesController');
+const app = express();
 
 // controllers routes:
-app.use(`${apiUrl}get-user`, userController);
-app.use(`${apiUrl}get-categories`, categoriesController);
+app.use(`${apiUrl}get-user`, require('./routes/user'));
+app.use(`${apiUrl}get-categories`, require('./routes/categories'));
 
 // listen on port
 app.listen(port, () => console.log(`Server is running on http://localhost:${port}`));
