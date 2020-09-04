6f307e9104f53854a79117191f04758d059e0ddc
rayvector <rayvectorspqr@yahoo.com>
2020-09-04 17:14:07 +0300

    cors enabled

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const config = require('./configs/main');
 const {port, apiUrl} = config;
+const cors = require('cors');
 
 const app = express();
 
+app.use(cors());
 // controllers routes:
 app.use(`${apiUrl}get-user`, require('./routes/user'));
 app.use(`${apiUrl}get-categories`, require('./routes/categories'));
